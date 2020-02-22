5a28e7f7055d50f8bcb27a0fb90302815bc1f3be
choral427 <tara4027@naver.com>
2020-02-23 01:37:00 +0900

    header 클릭시 홈 화면으로 돌아가기 수정 완료

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = () => {
     return (
         <header>
-            <h1><a href="#">마물달리기</a></h1>
+            <h1><a href="/">마물달리기</a></h1>
         </header>
     )
 }
